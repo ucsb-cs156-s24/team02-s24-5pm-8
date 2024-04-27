b5c6806da1f7b31658ef2f97ea188c9ba00024c9
Karsten Lansing <karstenlansing@ucsb.edu>
2024-04-27 12:07:51 -0700

    test

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -7,12 +7,11 @@ import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import liquibase.pro.packaged.R;
+import liquibase.pro.packaged.d;
 import lombok.extern.slf4j.Slf4j;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -29,33 +28,36 @@ import javax.validation.Valid;
 
 import java.time.LocalDateTime;
 
-@Tag(name = "RecommendationRequest")
-@RequestMapping("/api/recommendationrequest")
+@Tag(name = "RecommendationRequests")
+@RequestMapping("/api/recommendationrequests")
 @RestController
 @Slf4j
 public class RecommendationRequestController extends ApiController {
+    
+
     @Autowired
     RecommendationRequestRepository recommendationRequestRepository;
 
-    @Operation(summary = "List all recommendation requests")
+    @Operation(summary= "List all recommendation requests")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
     public Iterable<RecommendationRequest> allRecommendationRequests() {
         Iterable<RecommendationRequest> requests = recommendationRequestRepository.findAll();
         return requests;
     }
-
+    
     @Operation(summary = "Get a single recommendation Request")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("")
-    public RecommendationRequest getById(@Parameter(name = "id") @RequestParam Long id) {
+    public RecommendationRequest getById(@Parameter(name = "id") @RequestParam Long id){
         RecommendationRequest recommendationRequest = recommendationRequestRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
         return recommendationRequest;
+
     }
 
 
-    @Operation(summary = "Creates new recommendation request")
+    @Operation(summary= "Create a new recommendation request")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
     public RecommendationRequest postRecommendationDate(
@@ -67,20 +69,18 @@ public class RecommendationRequestController extends ApiController {
             @Parameter(name = "done") @RequestParam boolean done) 
             
             throws JsonProcessingException {
-        RecommendationRequest requestToPost = new RecommendationRequest();
-        
-        log.info("dateRequested:", dateRequested);
-        log.info("dateNeeded:", dateNeeded);
+        RecommendationRequest request = new RecommendationRequest();
 
-        requestToPost.setRequesterEmail(requesterEmail);
-        requestToPost.setProfessorEmail(professorEmail);
-        requestToPost.setExplanation(explanation);
-        requestToPost.setDateRequested(dateRequested);
-        requestToPost.setDateNeeded(dateNeeded);
-        requestToPost.setDone(done);
+        request.setRequesterEmail(requesterEmail);
+        request.setProfessorEmail(professorEmail);
+        request.setExplanation(explanation);
+        request.setDateRequested(dateRequested);
+        request.setDateNeeded(dateNeeded);
+        request.setDone(done);
 
-        RecommendationRequest savedRequest = recommendationRequestRepository.save(requestToPost);
+        RecommendationRequest savedRecommendationRequest = recommendationRequestRepository.save(request);
 
-        return savedRequest;
+        return savedRecommendationRequest;
     }
+    
 }
