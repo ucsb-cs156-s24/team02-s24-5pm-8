a503932074339bbfd044497025f62deba61bc713
Karsten James Lansing <karsten.lansing@gmail.com>
2024-05-01 20:33:50 -0700

    Revert "hs- Add PUT (edit) endpoint for a single record in HelpRequest table"

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {}
