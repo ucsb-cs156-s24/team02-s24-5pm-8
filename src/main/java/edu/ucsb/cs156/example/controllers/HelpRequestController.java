21541b8435cd8112177929a25c7f6f0c4c8a1abd
Hannah Shakouri <hannahshakouri@169-231-17-78.wireless.ucsb.edu>
2024-05-01 19:16:42 -0700

    hs- added single get

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
@@ -36,73 +36,43 @@ public class HelpRequestController extends ApiController {
     @Autowired
     HelpRequestRepository helpRequestRepository;
 
-    @Operation(summary= "List all help requests")
+    @Operation(summary = "List all help requests")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
     public Iterable<HelpRequest> allHelpRequests() {
-        Iterable<HelpRequest> helpRequests = helpRequestRepository.findAll();
-        return helpRequests;
+        return helpRequestRepository.findAll();
     }
 
-    @Operation(summary= "Create a new help request")
+    @Operation(summary= "Get a single help request")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public HelpRequest getById(
+            @Parameter(name="id") @RequestParam Long id) {
+        return helpRequestRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(HelpRequest.class, id));
+    }
+
+    @Operation(summary = "Create a new help request")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
     public HelpRequest postHelpRequest(
             @Parameter(name="requesterEmail") @RequestParam String requesterEmail,
             @Parameter(name="teamId") @RequestParam String teamId,
             @Parameter(name="tableOrBreakoutRoom") @RequestParam String tableOrBreakoutRoom,
-            @Parameter(name="requestTime") @RequestParam("requestTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestTime,
+            @Parameter(name="requestTime", description = "in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601") @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime requestTime,
             @Parameter(name="explanation") @RequestParam String explanation,
-            @Parameter(name="solved") @RequestParam boolean solved)
-            throws JsonProcessingException {
-    
-    log.info("requestTime={}", requestTime);
-
-        HelpRequest helpRequest = new HelpRequest();
-        helpRequest.setRequesterEmail(requesterEmail);
-        helpRequest.setTeamId(teamId);
-        helpRequest.setTableOrBreakoutRoom(tableOrBreakoutRoom);
-        helpRequest.setRequestTime(requestTime);
-        helpRequest.setExplanation(explanation);
-        helpRequest.setSolved(solved);
-
-        HelpRequest savedHelpRequest = helpRequestRepository.save(helpRequest);
-
-        return savedHelpRequest;
+            @Parameter(name="solved") @RequestParam boolean solved
+    ) throws JsonProcessingException {
+        var helpRequest = HelpRequest.builder()
+                .requesterEmail(requesterEmail)
+                .teamId(teamId)
+                .tableOrBreakoutRoom(tableOrBreakoutRoom)
+                .requestTime(requestTime)
+                .explanation(explanation)
+                .solved(solved)
+                .build();
+        return helpRequestRepository.save(helpRequest);
     }
 
-    @Operation(summary= "Delete a HelpRequest")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @DeleteMapping("")
-    public Object deleteHelpRequest(
-            @Parameter(name="id") @RequestParam Long id) {
-        HelpRequest helpRequest = helpRequestRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(HelpRequest.class, id));
-
-        helpRequestRepository.delete(helpRequest);
-        return genericMessage("HelpRequest with id %s deleted".formatted(id));
-    }
-
-    @Operation(summary= "Update a single help request")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PutMapping("")
-    public HelpRequest updateHelpRequest(
-            @Parameter(name="id") @RequestParam Long id,
-            @RequestBody @Valid HelpRequest incoming) {
-
-        HelpRequest helpRequest = helpRequestRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(HelpRequest.class, id));
-
-        helpRequest.setRequesterEmail(incoming.getRequesterEmail());
-        helpRequest.setTeamId(incoming.getTeamId());
-        helpRequest.setTableOrBreakoutRoom(incoming.getTableOrBreakoutRoom());
-        helpRequest.setRequestTime(incoming.getRequestTime());
-        helpRequest.setExplanation(incoming.getExplanation());
-        helpRequest.setSolved(incoming.getSolved());
-
-        helpRequestRepository.save(helpRequest);
-
-        return helpRequest;
-    }
 
 }
